fd0f28a667fb1803f62e64fa84fb126db2ad15d8
unknown <shahanavakkarath@gmail.com>
2025-08-07 15:12:13 +0530

    finallll

--- src/admin/Products.jsx
+++ src/admin/Products.jsx
@@ -162,7 +162,8 @@ const ProductsAdmin = () => {
 
  
       {showDeleteModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 backdrop-blur-sm backdrop-brightness-50 flex items-center justify-center z-50">
+         {/* <div className="fixed inset-0 bg-black bg-opacity-50  flex items-center justify-center z-50"> */}
           <div className="bg-white rounded-lg shadow-lg p-6 w-80 animate-fade-in">
             <h3 className="text-lg font-semibold mb-4">Confirm Delete</h3>
             <p className="text-sm mb-4 text-gray-700">
